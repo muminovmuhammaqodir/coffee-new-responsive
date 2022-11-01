01074e35c1600ed4af68a3cea9da88b27538fc50
muhammad1749 <muhammadqodirmuminov2@gmail>
2022-11-01 16:19:46 +0300

    modal-upload

--- main.js
+++ main.js
@@ -9,3 +9,15 @@ hamburgerMenu.addEventListener("click", function () {
 close.addEventListener("click", function () {
 	mobile.classList.remove("mobile--active");
 });
+
+// modal
+var modal = document.querySelector("#modalbtn");
+var modalMain = document.querySelector(".modal");
+var modalClose = document.querySelector("#modalcls");
+
+modal.addEventListener("click", function () {
+	modalMain.classList.toggle("modal-active");
+});
+modalClose.addEventListener("click", function () {
+	modalMain.classList.remove("modal-active");
+});
